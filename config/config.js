08954598838f137f96b7b82e957607ecfddcf67a
Miglena Pencheva <miglena_pencheva@abv.bg>
2021-03-15 07:27:56 +0200

    initial

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const DATABASE_NAME = '';
+
+const config = {
+    PORT: 6000,
+    DB_URI: `mongodb://localhost/${DATABASE_NAME}`,
+    SALT_ROUNDS: 10,
+    SECRET: 'STAVAMNOGOSOLENO',
+    COOKIE_NAME: 'TOKEN',
+};
+
+module.exports = config;
